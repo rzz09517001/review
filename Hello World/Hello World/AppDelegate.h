b4f38d3ef2dfc75d52f31b258279ef62d55e4de0
rzz <rzz09517001@163.com>
2017-06-14 21:00:59 +0800

    第一次提交

---
+++ Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by macOs on 2017/6/14.
+//  Copyright © 2017年 rzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
