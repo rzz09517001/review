0b4b915d7faf47597871e98f9568772d1995b794
rzz <rzz09517001@163.com>
2017-06-16 15:10:27 +0800

    第5章1
    界面旋转

---
+++ Orientations/Orientations/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Orientations
+//
+//  Created by macOs on 2017/6/16.
+//  Copyright © 2017年 rzz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return (UIInterfaceOrientationMaskPortrait |
+            UIInterfaceOrientationMaskLandscapeLeft);
+}
+@end
